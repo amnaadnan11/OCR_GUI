358eeb26158b6432bc5cefdd45084d8c929c7871
Akashdeep Singh <akash02ita@gmail.com>
2023-05-28 14:12:20 -0600

    run_tensorflow_model working -- logical error same probability array

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import base64
 import io
 from PIL import Image
+import numpy as np
 
 
 model = load_model('model.h5')
@@ -14,14 +15,22 @@ def run_tensorflow_model(drawing_data):
     
     # Read the image from bytes
     image = Image.open(io.BytesIO(image_bytes))
+
+    # Convert the image to grayscale
+    grayscale_image = image.convert('L')
     
     # Preprocess the image
-    resized_image = image.resize((28, 28))
+    # resized_image = image.resize((28, 28)) # this one gives error due to larger shape (grayscale conversion reduces dimension)
+    resized_image = grayscale_image.resize((28, 28))
     image_array = tf.keras.preprocessing.image.img_to_array(resized_image)
     image_array = image_array / 255.0  # Normalize pixel values to [0, 1]
     processed_image = tf.expand_dims(image_array, axis=0)
     
     # Perform prediction
     result = model.predict(processed_image)
+
+    probs = result[0].tolist()
+    print(probs)
+    digit = probs.index(max(probs))
     
-    return str(result[0])
+    return str(digit)
