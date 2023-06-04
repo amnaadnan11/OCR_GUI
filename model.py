517229fe79445f65a8eacdacd71900749f3882f1
Akashdeep Singh <akash02ita@gmail.com>
2023-06-04 12:52:22 -0600

    model.py -- black image bug fixed

--- model.py
+++ model.py
@@ -16,9 +16,10 @@ def run_tensorflow_model(drawing_data):
     # Read the image from bytes
     image = Image.open(io.BytesIO(image_bytes))
     image.save("./imageRGBA.png")
-    image = image.convert("RGB") # black -> problem -> transparency data is lost when converting to grayscale!
-    image.save("./imageRGB.png")
-    print(f"image.mode is {image.mode}")
+
+    # apply filter so that transparency effects are not lost in conversion
+    background = Image.new("RGB", image.size, (255, 255, 255))
+    image = Image.alpha_composite(background.convert("RGBA"), image)
 
     # Convert the image to grayscale
     grayscale_image = image.convert("L")
@@ -27,6 +28,7 @@ def run_tensorflow_model(drawing_data):
     # Preprocess the image
     # resized_image = image.resize((28, 28)) # this one gives error due to larger shape (grayscale conversion reduces dimension)
     resized_image = grayscale_image.resize((28, 28))
+    resized_image.save("./grayresized.png")
     image_array = tf.keras.preprocessing.image.img_to_array(resized_image)
     image_array = image_array / 255.0  # Normalize pixel values to [0, 1]
     processed_image = tf.expand_dims(image_array, axis=0)
